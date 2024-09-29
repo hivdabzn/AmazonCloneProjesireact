d8d259032d12fa636ade9b08e348aa1890f357e8
seliinatmaca <selinatmacayy@gmail.com>
2024-09-30 00:52:35 +0300

    first commit

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import { View, Text, SafeAreaView, StatusBar } from "react-native";
+import Router from "./src/navigation/Router";
+
+const App = () => {
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#9ee4d4" }}>
+      <StatusBar barStyle={"dark-content"} />
+      <Router />
+    </SafeAreaView>
+  );
+};
+export default App;
